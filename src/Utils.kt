bc2c185a25e0c8441c41af9cf9acad633d11ab45
Allen <allen.gueco1@gmail.com>
2021-12-07 22:51:05 -0500

    add util fn to read as int

--- src/Utils.kt
+++ src/Utils.kt
@@ -8,6 +8,11 @@ import java.security.MessageDigest
 fun readInput(name: String) = File("src", "$name.txt").readLines()
 
 /**
+ * Reads lines and parse into int
+ */
+fun readInputAsInts(name: String) = readInput(name).map(String::toInt)
+
+/**
  * Converts string to md5 hash.
  */
 fun String.md5(): String = BigInteger(1, MessageDigest.getInstance("MD5").digest(toByteArray())).toString(16)
