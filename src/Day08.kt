18960e75c05d2ecfc2d09493df58f749d14d5b63
Allen <allen.gueco1@gmail.com>
2021-12-19 18:14:47 -0500

    day8 done

--- src/Day08.kt
+++ src/Day08.kt
@@ -18,99 +18,86 @@ fun main() {
     }
 
     fun part2(input: List<String>): Int {
-        return 0
+        return Day08(input).solve2()
     }
 
     val input = readInput("Day08")
     println(part1(input))
-//    println(part2(input))
+    println(part2(input))
 }
 
 class Mapping {
-    var digitMapping: MutableMap<Int, SortedSet<Char>> = mutableMapOf()
-    var lengthMapping: MutableMap<String, Int> = mutableMapOf()
-    var outputDigits = listOf<String>()
+    var digitMapping: MutableMap<SortedSet<Char>, Int?> = mutableMapOf()
+    var outputDigits = listOf<SortedSet<Char>>()
 
     companion object {
         fun fromString(list: String): Mapping {
             val mapping = Mapping()
             val input = list.split("|")
 
-            mapping.outputDigits = input[1].trim().split(" ").map { it.trim() }
-            mapping.addAll(input[0])
+            mapping.outputDigits = input[1].trim().split(" ").map { it.trim().toSortedSet() }
 
-            mapping.determineDigitMapping()
+            mapping.determineDigitMapping(input[0].split(" "))
 
             return mapping
         }
+
+        private fun containsSegment(s1: SortedSet<Char>, s2: SortedSet<Char>) = s1.containsAll(s2)
     }
 
-    fun addAll(segments: String) {
-        segments.trim().split(" ").forEach { put(it) }
+    private fun getKeyByValue(value: Int): SortedSet<Char> {
+        return digitMapping.filterValues { it == value }.keys.first()
     }
 
-    private fun put(segment: String) {
-        val key = filtering(segment)
-        val value = key.length
-
-
-        lengthMapping[key] = key.length
-    }
-
-    private fun filtering(segment: String) = segment.toSortedSet().joinToString("")
 
     /**
      * What actually solves part 2
      */
-    private fun determineDigitMapping() {
-        for ((digit, characters) in digitMapping) {
-            // Obvious cases
-            if (characters.size == 2) {
-                digitMapping[1] = characters
-            }
-            if (characters.size == 3) {
-                digitMapping[7] = characters
-            }
-            if (characters.size == 4) {
-                digitMapping[4] = characters
-            }
-            if (characters.size == 7) {
-                digitMapping[8] = characters
-            }
+    private fun determineDigitMapping(segments: List<String>) {
 
-            when (characters.size) {
-                // This could be a 5, 2, or 3
-                5 -> {
-                    // It's a `3` if its characters contain all the characters in `1`
-                    if (characters.containsAll(digitMapping[1]!!)) {
-                        digitMapping[3] = characters
-                    }
+        fun determineObvious() {
+            val characters = segments.filter { s -> listOf(2, 3, 4, 7).any { it == s.length } }
 
-                }
-                // This could be a 9, 6, 0
-                6 -> {
-                    // excluding 8, only `4` is contained in 9.
-                    if (characters.containsAll(digitMapping[4]!!)) {
-                        digitMapping[9] = characters
-                    }
-
-                    if (characters.containsAll(digitMapping[1]!!)) {
-                        digitMapping[0] = characters
-                    }
-                    else {
-                        digitMapping[6] = characters
-                    }
-
+            for (char in characters) {
+                val s = char.toSortedSet()
+                when (char.length) {
+                    2 -> digitMapping[s] = 1
+                    3 -> digitMapping[s] = 7
+                    4 -> digitMapping[s] = 4
+                    7 -> digitMapping[s] = 8
                 }
             }
         }
+        fun determineSixSegments() {
+            val sixSegments = segments.filter { it.length == 6 }
+
+            for (char in sixSegments) {
+                val s = char.toSortedSet()
+                when {
+                    containsSegment(s, getKeyByValue(4)) -> digitMapping[s] = 9
+                    containsSegment(s, getKeyByValue(7)) -> digitMapping[s] = 0
+                    else -> digitMapping[s] = 6
+                }
+            }
+        }
+        fun determineFiveSegments() {
+            val fiveSegments = segments.filter { it.length == 5 }
+            for (char in fiveSegments) {
+                val s = char.toSortedSet()
+                when {
+                    containsSegment(s, getKeyByValue(7)) -> digitMapping[s] = 3
+                    containsSegment(getKeyByValue(6), s) -> digitMapping[s] = 5
+                    else -> digitMapping[s] = 2
+                }
+            }
+        }
+
+        determineObvious()
+        determineSixSegments()
+        determineFiveSegments()
     }
 
-    fun outputDigit() = outputDigits.map { this[it] }.joinToString("").toInt()
-
-    operator fun get(segment: String): Int {
-        TODO()
-    }
+    fun outputDigit() = outputDigits.map { this.digitMapping[it] }.joinToString("").toInt()
 }
 
 class Day08(val input: List<String>) {
